5891d78f9f343da6db98252dd9f5c81f1a1e70ac
ohdowon064 <dhehdnjs03@gmail.com>
2022-01-27 23:30:00 +0900

    Buffered Channel and Unbuffered Channel 버퍼 채널은 수신자가 없어도 고루틴 데드락 에러를 발생시키지않는다.

--- src/basic/ch19/19_channel.go
+++ src/basic/ch19/19_channel.go
@@ -47,23 +47,68 @@ func main() {
         done <- true
     }()
     
+    // 메인루틴은 고루틴이 끝날 때까지 대기
+    // 익명함수 고루틴에서 작업이 끝난 후, done채널에 true를 보내면
+    // 수신자 메인루틴은 이를 받고 프로그램을 종료한다.
+    <- done
 
-    done1 := make(chan bool)
-    done2 := make(chan bool)
 
-    go run1(done1)
-    go run2(done2)
+    /*
+        2. Go 채널 버퍼링
+        - Go 채널은 2가지가 있다.
+            1) Unburffered Channel
+            2) Buffered Channel
+        
+        - 위 예제는 Unbuffered Channel로 하나의 수신자가 데이터를 받을 때까지
+        - 송신자가 데이터를 보내는 채널에 묶여있게 된다.
 
-EXIT:
-    for {
-        select {
-        case <-done1:
-            println("run1 완료")
-        case <-done2:
-            println("run2 완료")
-            break EXIT
+        - 하지만 Buffered Channel을 사용하면 비록 수신자가 받을 준비가 안돼도
+        - 지정된 버퍼만큼 데이터를 보내고 계속 다른 일을 수행할 수 있다.
+        - 버퍼 채널은 make(chan type, N)함수를 통해 생성한다.
+            - N는 사용할 버퍼 개수를 의미한다.
+            - make(chan int, 10)는 10개의 정수형을 갖는 버퍼채널이다.
+        
+        - 고루틴 데드락 예제
+        - (fatal error: all goroutines are asleep - deadlock!)
+        func main() {
+            c := make(chan int)
+
+            // 현재 이곳은 메인루틴이다.
+            // 메인루틴에서 채널로 1을 전송
+            c <- 1 // 수신 루틴이 없으므로 데드락
+            
+            fmt.Println(<-c) // 코멘트(주석처리)해도 데드락, 별도의 고루틴이 없기때문
         }
-    }
+        // 위 코드는 메인루틴에서 채널에 1을 보내면서 상대 수신자를 기다리는데
+        // 이 채널을 받는 수신자 고루틴이 없다.
+
+        - 하지만 버퍼채널을 사용하면 당장 수신자가 없더라도
+        - 최대 버퍼수까지 데이터를 보낼 수 있다.
+    */
+
+    bufferedCh := make(chan int, 1)
+    
+    // 수신자가 없더라도 보낼 수 있다.
+    bufferedCh <- 101
+    fmt.Println(<-bufferedCh)
+
+
+//     done1 := make(chan bool)
+//     done2 := make(chan bool)
+
+//     go run1(done1)
+//     go run2(done2)
+
+// EXIT:
+//     for {
+//         select {
+//         case <-done1:
+//             println("run1 완료")
+//         case <-done2:
+//             println("run2 완료")
+//             break EXIT
+//         }
+//     }
 }
 
 func run1(done chan bool) {
